6e078c55cf6c130b8f1accf429ef96911143b8d4
denisYordanov-eng <denisivov437@gmail.com>
2025-04-11 14:10:08 +0300

    Made a task/

---
+++ 01-CountRealNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+            var counts = new SortedDictionary<double, int>();
+
+            foreach (var item in numbers)
+            {
+                if (counts.ContainsKey(item))
+                {
+                    counts[item]++;
+                }
+                else
+                {
+                    counts[item] = 1;
+                }
+                
+            }
+           foreach (var num in counts.Keys)
+            {
+                Console.WriteLine($"{num} ->{counts[num]}");
+            }
+        }
+    }
+}
