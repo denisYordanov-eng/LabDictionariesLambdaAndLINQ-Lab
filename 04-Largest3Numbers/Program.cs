2a3b9fd56cad7ceba882527e52a34699393cb0d8
denisYordanov-eng <denisivov437@gmail.com>
2025-04-12 10:22:45 +0300

    Made a task.

---
+++ 04-Largest3Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Largest3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            var orderList = list.OrderByDescending(x => x).Take(3);
+
+            Console.WriteLine(String.Join(" ", orderList));
+        }
+    }
+}
