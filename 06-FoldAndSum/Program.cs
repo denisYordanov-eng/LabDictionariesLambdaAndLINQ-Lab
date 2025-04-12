01f4fa160cc947fa7dd7e7c3d009a14c7fe691fa
denisYordanov-eng <denisivov437@gmail.com>
2025-04-12 14:34:59 +0300

    Made a task.

---
+++ 06-FoldAndSum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_FoldAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int k = input.Length / 4;
+
+            int[] leftArr = input.Take(k).Reverse().ToArray();
+            int[] rightArr = input.Reverse().Take(k).ToArray();
+
+            int[] leftSide = leftArr.Concat(rightArr).ToArray();
+
+            int[] rightSide = input.Skip(k).Take(2 * k).ToArray();
+
+            var sum = leftSide.Select((x,index) => x + rightSide[index]);
+
+            Console.WriteLine(String.Join(" ", sum));
+        }
+    }
+}
