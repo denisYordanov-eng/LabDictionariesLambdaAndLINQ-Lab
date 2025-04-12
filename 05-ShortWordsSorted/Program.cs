13d3c036cccfef21edf31b95b1c3c4ffb5bcfa1e
denisYordanov-eng <denisivov437@gmail.com>
2025-04-12 14:01:25 +0300

    Made a task.

---
+++ 05-ShortWordsSorted/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_ShortWordsSorted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //. , : ; ( )[ ] " ' \ / ! ? (space).
+            char[] separators = { ' ', '.', ':', ';', '(', ')', '/', '\\', '!', '?', ',' };
+            string[] text = Console.ReadLine()
+                .ToLower()
+                .Split(separators)
+                .ToArray();
+
+            var result = text
+            .Where(w => w != "")
+            .Where(w => w.Length < 5)
+            .OrderBy(w => w)
+            .Distinct();
+
+            Console.WriteLine(String.Join(", ", result));
+        }
+    }
+}
