f82e13534e36300981a016781c02168c216aabec
denisYordanov-eng <denisivov437@gmail.com>
2025-04-12 10:04:56 +0300

    Made a task.

---
+++ 03-SumMinMaxAverage/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_SumMinMaxAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] num = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+              num[i] = int.Parse(Console.ReadLine());
+            }
+            int minValue = num.Min();
+            int maxValue = num.Max();
+            int sum = num.Sum();
+            double average = num.Average();
+
+            Console.WriteLine("Sum = " + sum);
+            Console.WriteLine("Min = " + minValue);
+            Console.WriteLine("Max = " + maxValue);
+            Console.WriteLine("Average = " + average);
+
+
+
+
+        }
+    }
+}
